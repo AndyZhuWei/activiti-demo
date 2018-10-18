40afedc0ce1c9a1bf07b1ed90c6a27c3e2c09def
AndyLG <843301191@qq.com>
2018-10-19 07:08:04 +0800

    添加测试类

--- src/test/java/cn/andy/activiti/designer/ProcessTestLeave.java
+++ src/test/java/cn/andy/activiti/designer/ProcessTestLeave.java
@@ -1,9 +1,17 @@
 package cn.andy.activiti.designer;
 
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.test.ActivitiRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @Author: zhuwei
@@ -12,8 +20,9 @@ import org.junit.Test;
  */
 public class ProcessTestLeave {
 
-    private String filename = "F:\\IT\\activiti-demo\\src\\main\\resources\\cn.andy.activiti\\diagrams\\Leave.bpmn";
+//    private String filename = "F:\\IT\\activiti-demo\\src\\main\\resources\\cn.andy.activiti\\diagrams\\Leave.bpmn";
 
+    private String filename = "E:\\workspace\\activiti-demo\\src\\main\\resources\\cn.andy.activiti\\diagrams\\Leave.bpmn";
     @Rule
     public ActivitiRule activitiRule = new ActivitiRule();
 
@@ -21,7 +30,15 @@ public class ProcessTestLeave {
     public void startProcess() throws Exception {
         RepositoryService repositoryService = activitiRule.getRepositoryService();
 
-//        repositoryService.createDeployment().addInputStream();
+       repositoryService.createDeployment().addInputStream(
+               "leave.bpmn20.xml",new FileInputStream(filename)).deploy();
+
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        Map<String,Object> variableMap = new HashMap<String,Object>();
+        variableMap.put("name","Activiti");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("leave",variableMap);
+        assertNotNull(processInstance.getId());
+        System.out.println("id"+processInstance.getId()+" "+processInstance.getProcessDefinitionId());
     }
 
 
